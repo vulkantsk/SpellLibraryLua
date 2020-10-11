aab108729f54862c5bccfd2d14d31aa886a8bc88
fdsatrew <fdsatrewq3@gmail.com>
2020-10-11 14:26:33 +0300

    Working weaver 1st skill

--- game/SpellLibraryLua/scripts/vscripts/heroes/weaver/the_swarm.lua
+++ game/SpellLibraryLua/scripts/vscripts/heroes/weaver/the_swarm.lua
@@ -1,23 +1,12 @@
-LinkLuaModifier("modifier_the_swarm_unit", "heroes/weaver/the_swarm", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_the_swarm_beetle", "heroes/weaver/the_swarm", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_the_swarm_debuff", "heroes/weaver/the_swarm", LUA_MODIFIER_MOTION_NONE)
 
 ability_the_swarm = class({})
 
-function ability_the_swarm:OnUpgrade()
-    self.damage = self:GetSpecialValueFor("damage")
-    self.attack_rate = self:GetSpecialValueFor("attack_rate")
-    self.count = self:GetSpecialValueFor("count")
-    self.armor_reduction = self:GetSpecialValueFor("armor_reduction")
-    self.duration = self:GetSpecialValueFor("duration")
-    self.destroy_attacks = self:GetSpecialValueFor("destroy_attacks")
-    self.radius = self:GetSpecialValueFor("radius")
-    self.speed = self:GetSpecialValueFor("speed")
-    self.spawn_radius = self:GetSpecialValueFor("spawn_radius")
-    self.travel_time = self:GetSpecialValueFor("travel_time")
-    self.damage_type = self:GetAbilityDamageType()
-end
-
 function ability_the_swarm:OnSpellStart()
+    if(not IsServer()) then
+        return
+    end
     self.caster = self:GetCaster()
     local cursor_position = self:GetCursorPosition()
     local caster_position = self.caster:GetAbsOrigin()
@@ -29,10 +18,10 @@ function ability_the_swarm:OnSpellStart()
     if RollPercentage(75) then
         self.caster:EmitSound("weaver_weav_ability_swarm_0" .. RandomInt(1, 6))
     end
-    local start_position, thinker, projectile, projectile_id
-    for beetles = 1, self.count do
-        start_position = caster_position + RandomVector(RandomInt(0, self.spawn_radius))
-        thinker = CreateModifierThinker(
+    local start_position, projectile, projectile_id
+    local speed = self:GetSpecialValueFor("speed")
+    for beetles = 1, self:GetSpecialValueFor("count") do
+        local thinker = CreateModifierThinker(
                 self.caster,
                 self,
                 nil,
@@ -40,11 +29,12 @@ function ability_the_swarm:OnSpellStart()
                 caster_position,
                 self.caster_team,
                 false)
+        start_position = caster_position + RandomVector(RandomInt(0, self:GetSpecialValueFor("spawn_radius")))
         projectile = {
             Ability = self,
             EffectName = "particles/units/heroes/hero_weaver/weaver_swarm_projectile.vpcf",
             vSpawnOrigin = start_position,
-            fDistance = (self.speed * self.travel_time) + self.caster:GetCastRangeBonus(),
+            fDistance = (speed * self:GetSpecialValueFor("travel_time")) + self.caster:GetCastRangeBonus(),
             fStartRadius = self.radius,
             fEndRadius = self.radius,
             Source = self.caster,
@@ -55,7 +45,7 @@ function ability_the_swarm:OnSpellStart()
             iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
             fExpireTime = GameRules:GetGameTime() + 10.0,
             bDeleteOnHit = false,
-            vVelocity = (cursor_position - caster_position):Normalized() * self.speed * Vector(1, 1, 0),
+            vVelocity = (cursor_position - caster_position):Normalized() * speed * Vector(1, 1, 0),
             bProvidesVision = true,
             iVisionRadius = 321,
             iVisionTeamNumber = self.caster_team,
@@ -72,7 +62,7 @@ function ability_the_swarm:OnSpellStart()
 end
 
 function ability_the_swarm:OnProjectileThink_ExtraData(location, data)
-    if (not IsServer()) then
+    if(not IsServer()) then
         return
     end
     local thinker = EntIndexToHScript(data.thinker_index)
@@ -82,42 +72,51 @@ function ability_the_swarm:OnProjectileThink_ExtraData(location, data)
 end
 
 function ability_the_swarm:OnProjectileHit_ExtraData(target, location, data)
-    local thinker = EntIndexToHScript(data.thinker_index)
-    local thinker_alive = thinker and not thinker:IsNull()
-    if (target and not target:HasModifier("modifier_the_swarm_debuff") and thinker_alive) then
-        target:EmitSound("Hero_Weaver.SwarmAttach")
-        local target_position = target:GetAbsOrigin()
-        local beetle = CreateUnitByName("npc_dota_weaver_swarm", target_position + target:GetForwardVector() * 64, false, self.caster, self.caster, self.caster_team)
-        beetle:AddNewModifier(
-                self.caster,
-                self,
-                "modifier_the_swarm_unit",
-                {
-                    target_entindex = target:entindex(),
-                    destroy_attacks = self.destroy_attacks,
-                }
-        )
-        target:AddNewModifier(
-                self.caster,
-                self,
-                "modifier_the_swarm_debuff",
-                {
-                    duration = self.duration,
-                    beetle_entindex = beetle:entindex()
-                }
-        )
-        beetle:SetForwardVector((target_position - beetle:GetAbsOrigin()):Normalized())
-        if (thinker.projectile_id) then
-            ProjectileManager:DestroyLinearProjectile(thinker.projectile_id)
-        end
+    if(not IsServer()) then
+        return
     end
-    if (thinker_alive) then
+    local thinker = EntIndexToHScript(data.thinker_index)
+    if (target) then
+        if (not target:HasModifier("modifier_the_swarm_debuff")) then
+            target:EmitSound("Hero_Weaver.SwarmAttach")
+            local target_position = target:GetAbsOrigin()
+            local beetle = CreateUnitByName(
+                    "npc_dota_weaver_swarm",
+                    target_position + target:GetForwardVector() * 64,
+                    false,
+                    self.caster,
+                    self.caster,
+                    self.caster_team
+            )
+            beetle:AddNewModifier(
+                    self.caster,
+                    self,
+                    "modifier_the_swarm_beetle",
+                    {
+                        target_entindex = target:entindex()
+                    }
+            )
+            target:AddNewModifier(
+                    self.caster,
+                    self,
+                    "modifier_the_swarm_debuff",
+                    {
+                        beetle_entindex = beetle:entindex(),
+                        duration = self:GetSpecialValueFor("duration")
+                    }
+            )
+            beetle:SetForwardVector((target_position - beetle:GetAbsOrigin()):Normalized())
+            ProjectileManager:DestroyLinearProjectile(thinker.projectile_id)
+            thinker:StopSound("Hero_Weaver.Swarm.Projectile")
+            UTIL_Remove(thinker)
+        end
+    else
         thinker:StopSound("Hero_Weaver.Swarm.Projectile")
         UTIL_Remove(thinker)
     end
 end
 
-modifier_the_swarm_unit = class({
+modifier_the_swarm_beetle = class({
     IsHidden = function()
         return true
     end,
@@ -154,20 +153,19 @@ modifier_the_swarm_unit = class({
     end
 })
 
-function modifier_the_swarm_unit:OnCreated(params)
-    if (not IsServer()) then
+function modifier_the_swarm_beetle:OnCreated(params)
+    if(not IsServer()) then
         return
     end
-    self.ability = self:GetAbility()
     self.target = EntIndexToHScript(params.target_entindex)
     self.beetle = self:GetParent()
-    self.destroy_attacks = params.destroy_attacks
-    self.health_increments = self.beetle:GetMaxHealth() / self.destroy_attacks
-    self.hero_attack_multiplier = 2
+    self.ability = self:GetAbility()
+    self.health_increments = self.beetle:GetMaxHealth() / self.ability:GetSpecialValueFor("destroy_attacks")
+    self.hero_attack_multiplier = self.ability:GetSpecialValueFor("hero_attack_multiplier")
     self:StartIntervalThink(FrameTime())
 end
 
-function modifier_the_swarm_unit:OnIntervalThink()
+function modifier_the_swarm_beetle:OnIntervalThink()
     if (self.target and not self.target:IsNull()) then
         if (self.target:IsInvisible() and not self.beetle:CanEntityBeSeenByMyTeam(self.target)) or self.beetle:HasModifier("modifier_faceless_void_chronosphere_freeze") then
             self.beetle:ForceKill(false)
@@ -180,21 +178,15 @@ function modifier_the_swarm_unit:OnIntervalThink()
     end
 end
 
-function modifier_the_swarm_unit:OnDestroy()
-    if not IsServer() then
-        return
-    end
+function modifier_the_swarm_beetle:OnDestroy()
     if self.target and not self.target:IsNull() and self.target:HasModifier("modifier_the_swarm_debuff") then
         self.target:RemoveModifierByName("modifier_the_swarm_debuff")
     end
 end
 
-function modifier_the_swarm_unit:OnAttacked(keys)
-    if not IsServer() then
-        return
-    end
+function modifier_the_swarm_beetle:OnAttacked(keys)
     if (keys.target == self.beetle) then
-        local newBeetleHp = self.beetle:GetHealth() - (self.health_increments * self.hero_attack_multiplier)
+        local newBeetleHp = self.beetle:GetHealth()
         if (keys.attacker:IsHero()) then
             newBeetleHp = newBeetleHp - (self.health_increments * self.hero_attack_multiplier)
         else
@@ -229,37 +221,35 @@ modifier_the_swarm_debuff = class({
 })
 
 function modifier_the_swarm_debuff:OnCreated(params)
-    if not IsServer() then
+    self.ability = self:GetAbility()
+    self.armor_reduction = self.ability:GetSpecialValueFor("armor_reduction") * (-1)
+    if(not IsServer()) then
         return
     end
     self.beetle = EntIndexToHScript(params.beetle_entindex)
-    self.target = self:GetParent()
-    self.ability = self:GetAbility()
-    self:OnIntervalThink()
-    self:StartIntervalThink(self.ability.attack_rate)
-end
-
-function modifier_the_swarm_debuff:OnIntervalThink()
-    self:IncrementStackCount()
-    ApplyDamage {
-        victim = self.target,
-        damage = self.ability.damage,
-        damage_type = self.ability.damage_type,
+    self.damage_table = {
+        victim = self:GetParent(),
+        damage = self.ability:GetSpecialValueFor("damage"),
+        damage_type = self.ability:GetAbilityDamageType(),
         damage_flags = DOTA_DAMAGE_FLAG_NONE,
         attacker = self.ability.caster,
         ability = self.ability
     }
+    self:OnIntervalThink()
+    self:StartIntervalThink(self.ability:GetSpecialValueFor("attack_rate"))
+end
+
+function modifier_the_swarm_debuff:OnIntervalThink()
+    self:IncrementStackCount()
+    ApplyDamage(self.damage_table)
 end
 
 function modifier_the_swarm_debuff:OnDestroy()
-    if not IsServer() then
-        return
-    end
     if (self.beetle and not self.beetle:IsNull() and self.beetle:IsAlive()) then
         self.beetle:ForceKill(false)
     end
 end
 
 function modifier_the_swarm_debuff:GetModifierPhysicalArmorBonus()
-    return self.ability.armor_reduction * self:GetStackCount() * (-1)
+    return self.armor_reduction * self:GetStackCount()
 end
